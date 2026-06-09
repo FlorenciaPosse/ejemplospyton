dad6c85ac55f7b1a14d7f8b8221638ab60f19a7a
FlorenciaPosse <florenciaposse93@gmail.com>
2026-06-09 18:43:58 +0200

    Update t1e1peliculas.py

--- ejemplos/t1e1peliculas.py
+++ ejemplos/t1e1peliculas.py
@@ -1,17 +1,3 @@
-#Cadena
-nombre = "Flor"
-#Numeros (int)
-edad = 33
-#Numeros con decimales (float)
-pi = 3.14
-#Booleanos (True o False)
-gusta_python = False
-
-#Imprimir las variables
-print("Nombre", nombre)
-print("Edad ", edad)
-print("Pi ", pi)
-print("Me gusta python? ", gusta_python)
 
 #Obtener valores introducidos por el usuario
 
